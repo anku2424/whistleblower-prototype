82b9bdeed8e1f5086c4f10a02afa62a7ebc76b82
fouxan <fouzanfouzan1310@gmail.com>
2024-09-08 15:16:20 +0530

    fixed deployment logic

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,22 +1,6 @@
-// const fetchDocs = async (url, options) => {
-//     try {
-//         const response = await fetch(url, options);
-//         const data = await response.json();
-//         return data;
-//     } catch (error) {
-//         console.error("Error fetching data:", error);
-//     }
-// };
-
-// const fetchGPTAnswer = async (url, options) => {
-//     try {
-//         const response = await fetch(url, options);
-//         const data = await response.json();
-//         return data;
-//     } catch (error) {
-//         console.error("Error fetching data:", error);
-//     }
-// }
+import { useEffect } from "react";
+import { useCallback } from "react";
+import { useState } from "react";
 
 const dummyData = {
     docs: [
@@ -54,18 +38,34 @@ const dummyData = {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 };
 
-const fetchData = async (url, options) => {
-    console.log("url:", url);
-    console.log("options:", options);
-    try {
-        // const response = await fetch(url, options);
-        // const data = await response.json();
-        // setTimeout(() => {
-        return dummyData;
-        // }, 1000);
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
-};
+export const useFetchData = (formData, shouldFetch) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(null);
 
-export default fetchData;
+    console.log("useFetchData", shouldFetch);
+
+    const fetchData = useCallback(async () => {
+        if (!shouldFetch) return;
+        setIsLoading(true);
+        try {
+            const backend_url = process.env.BACKEND_URL;
+            console.log("backend_url", backend_url);
+            // const response = await fetch("https://api.example.com/data");
+            // const data = await response.json();
+            await new Promise((r) => setTimeout(r, 5000)); // Simulate delay
+            setData(dummyData);
+            // setData(data);
+        } catch (error) {
+            setIsError(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [shouldFetch]);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    return { data, isLoading, isError, reload: fetchData };
+};
